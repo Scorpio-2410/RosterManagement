49542cc712835c593988dc22379fb3ad1e42c169
Nabidul Islam <Nabidulislam90@gmail.com>
2024-06-04 22:05:16 +1000

    updated rosters into table format with search filter

--- website/src/routes/_auth/rosters.index.tsx
+++ website/src/routes/_auth/rosters.index.tsx
@@ -1,35 +1,35 @@
-import { createFileRoute} from '@tanstack/react-router'
-import { AppSettings } from '@/utils/configs'
+import { createFileRoute } from "@tanstack/react-router";
+import { AppSettings } from "@/utils/configs";
 
 type RosterResponse = {
-  rosterId: number
-  locationId: number
-  startingWeek: string
-}
+  rosterId: number;
+  locationId: number;
+  startingWeek: string;
+};
 
 type RosterSearchResponse = {
-  result: Array<RosterResponse>
-  totalRecords: number
-  pageSize: number
-  pageNumber: number
-}
+  result: Array<RosterResponse>;
+  totalRecords: number;
+  pageSize: number;
+  pageNumber: number;
+};
 
-
-export const Route = createFileRoute('/_auth/rosters/')({
+export const Route = createFileRoute("/_auth/rosters/")({
   loader: async () => {
-    return await GetRoster()
-  }
-})
+    return await GetRoster();
+  },
+});
 
 async function GetRoster() {
-  const response = await fetch(`${AppSettings.baseUrl}/rosters/search`,{
-    method:"POST",
-    body:JSON.stringify({pageSize:100}),
-    headers:{
-      "Content-Type": "application/json"
-    }
-  })
+  const response = await fetch(`${AppSettings.baseUrl}/rosters/search`, {
+    method: "POST",
+    body: JSON.stringify({ pageSize: 100 }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
   console.log(response);
-  return await response.json() as RosterSearchResponse;
-
+  return (await response.json()) as RosterSearchResponse;
 }
+
+export default GetRoster;
