08c571aff344dca95ac76aade86ce2741ed79173
Nabidul Islam <Nabidulislam90@gmail.com>
2024-06-05 21:58:21 +1000

    Fixed query & mutations

--- website/src/routes/_auth/users.lazy.tsx
+++ website/src/routes/_auth/users.lazy.tsx
@@ -1,60 +1,74 @@
-import { useQuery, useMutation } from '@tanstack/react-query';
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { createLazyFileRoute } from "@tanstack/react-router";
 import { AppSettings } from "@/utils/configs";
 
-export const Route = createLazyFileRoute('/_auth/users')({
-  component: Users
-})
+export const Route = createLazyFileRoute("/_auth/users")({
+  component: Users,
+});
 
 type User = {
-  id: number;
-  name: string;
+  userId?: number;
+  firstName: string;
+  lastName: string;
+  role: string;
+  availability?: string;
 };
 
 function Users() {
-  const {data, error, isLoading} = useQuery<User[]>({
-    queryKey: ["user"],
-    queryFn: () => fetch(`${AppSettings.baseUrl}/Users/search`).then((res) =>
-      res.json()
-    ),
+  const { data, error, isLoading, refetch } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch(`${AppSettings.baseUrl}/users/search`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          pageNumber: 1,
+          pageSize: 100,
+        }),
+      });
+      return await res.json();
+    },
   });
 
-const {mutate, isPending, isError} = useMutation({mutationFn: (user: User) => 
-  fetch(`${AppSettings.baseUrl}/Users/search`, {
-    method: "POST",
-    body: JSON.stringify(user),
-    headers: {"Content-type": "application/json; charset=UTF-8"},
-  }).then((res) => res.json()),
-});
+  const { mutate, isPending, isError } = useMutation({
+    mutationFn: async (user: User) => {
+      const res = await fetch(`${AppSettings.baseUrl}/users`, {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: { "Content-type": "application/json" },
+      });
+      return await res.json();
+    },
+    onSuccess: refetch,
+  });
 
   if (error || isError) return <div>There was an error!</div>;
 
-  if(isLoading) return <div>Data is Loading.....</div>;
+  if (isLoading) return <div>Data is Loading.....</div>;
+  console.log(data);
+  const handleSubmit = () => {
+    mutate({
+      firstName: "Nabidul",
+      lastName: "Islam",
+      role: "Student",
+    });
+  };
 
   return (
-    <div className='Dashboard'>
+    <div className="Dashboard">
       {/* Ispending is true and this will be shown */}
       {isPending && <p>Data is being added...</p>}
-      <button 
-        onClick={() => 
-          mutate({
-            id: 200,
-            name: "Nabidul Islam"
-          })
-        }
-      >
-        Add Post
-      </button>
-      {data && data.map((user) => (
-        <div>
-          {" "}
-          <h1>ID: {user.id}</h1>
-          <h1>Name: {user.name}</h1>
-        </div>
-      ))}
+      <button onClick={handleSubmit}>Add Post</button>
+      {data &&
+        data.result.map((user: User) => (
+          <div>
+            {" "}
+            <h1>ID: {user.userId}</h1>
+            <h1>Name: {user.firstName}</h1>
+          </div>
+        ))}
     </div>
   );
 }
-
-
-  
